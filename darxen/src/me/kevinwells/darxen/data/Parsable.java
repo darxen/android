1df0e7fee4e9be22d1dd459b236fc20400ad7bc1
Kevin Wells <wellsie1116@gmail.com>
2012-04-21 22:59:07 -0400

    Remove unused interface

--- darxen/src/me/kevinwells/darxen/data/Parsable.java
+++
@@ -1,8 +0,0 @@
-package me.kevinwells.darxen.data;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-public interface Parsable<T> {
-	public T parse(InputStream stream) throws ParseException, IOException;	
-}
